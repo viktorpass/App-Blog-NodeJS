7b4567259cc1e1b10d8ef3a8f19376be1a28045a
Viktor Furieri <79453555+viktorpass@users.noreply.github.com>
2022-04-28 00:13:40 -0300

    Update db.js

--- auth/db.js
+++ auth/db.js
@@ -1,5 +1,1 @@
-if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI:"mongodb+srv://viktor12:mydb1212@viktordb.njrpf.mongodb.net/ViktorDB?retryWrites=true&w=majority"}
-}else{
-    module.exports = {mongoURI:"mongodb://localhost/blogapp"}
-}
+
